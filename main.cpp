9dec4abd546d9e75913336b27f6728807c14e0a0
Tomasz Bonus <tbonus@makemake.galia.lan>
2016-05-23 17:40:20 +0200

    szabadaba

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 
 #include <iostream>
 #include <fstream>
+#include <iomanip>
+
 #include "draw.hpp"
 using namespace std;
 
@@ -16,13 +18,16 @@ using namespace std;
 #define I_min   0.5f
 #define I_max   0.6f
 
-#define R_center -0.22f
-#define I_center 0.64f
+#define R_center 0.001643721971153f
+#define I_center 0.822467633298876f
 #define wid 0.05f
-#define zoom 0.97f
+#define zoom 0.98f
 #define potega 2
 
-#define ile_klatek 120
+#define szybkosc 24
+#define dlugosc 30
+
+#define ile_klatek szybkosc * dlugosc
 #define iterations  500 + ile_klatek
 #define procent 5
 
@@ -30,14 +35,14 @@ using namespace std;
 #define picH    500
 #define color   255
 
-#define uR (R_max-R_min)/picW
-#define uI (I_max-I_min)/picH
+// #define uR (R_max-R_min)/picW
+// #define uI (I_max-I_min)/picH
 
 
 struct complex {
     float real;
     float imaginary;
-    
+
     complex operator +(const complex& b)
     {
         complex C;
@@ -45,7 +50,7 @@ struct complex {
         C.imaginary = this->imaginary + b.imaginary;
         return C;
     }
-    
+
     complex operator *(const complex& b)
     {
         complex C;
@@ -53,26 +58,39 @@ struct complex {
         C.imaginary = this->real*b.imaginary + this->imaginary*b.real;
         return C;
     }
-    
+
     complex operator ^(const unsigned& p)
     {
         complex tmp;
         tmp.real = this->real;
         tmp.imaginary = this->imaginary;
-        
+
         complex pot = tmp;
-        
-        for(int i=1; i<p; ++i)
+
+        for(unsigned int i=1; i<p; ++i)
             tmp  = tmp * pot;
-        
+
         return tmp;
     }
 };
 
-int findMandelbrot(complex num);
+int findMandelbrot(complex num)
+{
+    int i = 0;
+    complex z;
+    z.real =0;
+    z.imaginary = 0;
+
+    while (i < iterations && z.real*z.real + z.imaginary*z.imaginary < 2) {
+        z = (z^potega) + num;
+        ++i;
+    }
+
+    return i;
+}
 
 int main(int argc, char * argv[]) {
-    
+
     string name;
     if (argc < 2) {
         cout <<"Podaj nazwę tworzonych plików: ";
@@ -80,57 +98,57 @@ int main(int argc, char * argv[]) {
     } else {
         name = argv[1];
     }
-    
-    int setki = 0, dziesiatki = 0, jednosci = 0;
+
+    char setki = 48, dziesiatki = 48, jednosci = 48;
     float rozrzut = wid;
-    
+
     for (int klatki = 1; klatki <= ile_klatek; ++klatki)
     {
         float uR2 = rozrzut/picW;
         float uI2 = rozrzut/picH;
-        
+
         cout <<"Szerokość obrazka: " <<picW <<'\t' <<"Wysokość obrazka: " <<picH <<endl;
-        cout <<"Zakres części rzeczywistej: "   <<R_center - rozrzut <<" - " <<R_center + rozrzut <<endl;
-        cout <<"Zakres części urojonej: "       <<I_center - rozrzut <<" - " <<I_center + rozrzut <<endl;
-        
+        cout <<"Zakres części rzeczywistej: "   <<setprecision(20) <<R_center - rozrzut <<" - " <<R_center + rozrzut <<endl;
+        cout <<"Zakres części urojonej: "       <<setprecision(20) <<I_center - rozrzut <<" - " <<I_center + rozrzut <<endl;
+
         cout.sync_with_stdio(false);
-        
+
         string tmp = name;
-        
+
         if (ile_klatek > 1) {
-            tmp = name + setki + dziesiatki + jednosci + ".ppm";
+            tmp = "Grafiki/" + name + setki + dziesiatki + jednosci + ".ppm";
         } else {
-            tmp = name + ".ppm";
+            tmp = "Grafiki/" + name + ".ppm";
         }
-        
-        ofstream mandelbrot(tmp);
-        
+
+        ofstream mandelbrot(tmp.c_str());
+
         mandelbrot <<"P3" <<endl;
         mandelbrot <<picW <<' ' <<picH <<endl;
         mandelbrot <<color <<endl;
-        
+
         float postep = 0;
         for (int y=0; y<picH; ++y) {
             for (int x=0; x<picW; ++x) {
                 complex C;
                 C.real = R_center - rozrzut + 2*x*uR2;
                 C.imaginary = I_center - rozrzut + 2*y*uI2;
-                
+
                 //int n = findMandelbrot(C);
-                
+
                 draw pixel(findMandelbrot(C), mandelbrot, iterations);
                 //mandelbrot <<pixel.R <<' ' <<pixel.G <<' ' <<pixel.B <<'\n';
             }
-            
+
             if (y % (picH / 100 * procent) == 0) {
                 cout <<"Klatka " << klatki <<'\\' <<ile_klatek <<" - Rysowanie: "<<postep <<'%' <<'\n';
                 postep += procent;
             }
         }
         cout <<endl;
-        
+
         mandelbrot.close();
-        
+
         rozrzut *= zoom;
         jednosci++;
         if (jednosci == 58) {
@@ -140,37 +158,13 @@ int main(int argc, char * argv[]) {
         if (dziesiatki == 58) {
             dziesiatki -= 10;
             setki++;
-        
+	}
+
     }
-    
+
     cout.sync_with_stdio(true);
-    
+
     cout <<"Kuniec" <<endl;
-    
+
     return 0;
 }
-
-int findMandelbrot(complex num)
-{
-    int i = 0;
-    complex z;
-    z.real =0;
-    z.imaginary = 0;
-    
-    while (i < iterations && z.real*z.real + z.imaginary*z.imaginary < 2) {
-//        float tmp = z.real*z.real - z.imaginary*z.imaginary + num.real;
-//        z.imaginary = 2.f * z.real * z.imaginary + num.imaginary;
-//        z.real = tmp;
-        z = (z^potega) + num;
-        ++i;
-    }
-    
-    return i;
-}
-
-
-
-
-
-
-
