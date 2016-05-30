c711b2ecfd21e7b8e06d8a6437db8cc0d3944783
Bartosz Fidera <swidus6@interia.pl>
2016-05-30 21:43:00 +0200

    test palety

--- draw.hpp
+++ draw.hpp
@@ -11,6 +11,8 @@
 
 #include <fstream>
 
+#define palette 2
+
 class draw {    // klasa nadajaca kolor pixelom
 public:
     unsigned short int R;    // red
@@ -19,80 +21,141 @@ public:
 
 public:
     draw(float num, std::ofstream &mandelbrot, const int iterations)
-    {
-        if (num == iterations) {    // tu zaczyna sie paleta kolorow
-            this->R = 0;            // ktorej nie bede komentowal bo
-            this->G = 0;            // bo to nie ma sensu
-            this->B = 0;    // ale jesli pixel przetrwal wszystkie iteracje to koloruje go na czarno
-        } else {
-            num = (int)num % 16;
-            if (num < 1.f) {
-                this->R = 66;
-                this->G = 30;
-                this->B = 15;
-            } else if (num < 2.f) {
-                this->R = 25;
-                this->G = 7;
-                this->B = 26;
-            } else if (num < 3.f) {
-                this->R = 9;
-                this->G = 1;
-                this->B = 47;
-            } else if (num < 4.f) {
-                this->R = 4;
-                this->G = 4;
-                this->B = 73;
-            } else if (num < 5.f) {
-                this->R = 0;
-                this->G = 7;
-                this->B = 100;
-            } else if (num < 6.f) {
-                this->R = 12;
-                this->G = 44;
-                this->B = 138;
-            } else if (num < 7.f) {
-                this->R = 24;
-                this->G = 82;
-                this->B = 177;
-            } else if (num < 8.f){
-                this->R = 57;
-                this->G = 125;
-                this->B = 209;
-            } else if (num < 9.f) {
-                this->R = 134;
-                this->G = 181;
-                this->B = 229;
-            } else if (num < 10.f) {
-                this->R = 211;
-                this->G = 236;
-                this->B = 248;
-            } else if (num < 11.f) {
-                this->R = 241;
-                this->G = 233;
-                this->B = 191;
-            } else if (num < 12.f) {
-                this->R = 248;
-                this->G = 201;
-                this->B = 95;
-            } else if (num < 13.f) {
-                this->R = 255;
-                this->G = 170;
-                this->B = 0;
-            } else if (num < 14.f) {
-                this->R = 204;
-                this->G = 128;
-                this->B = 0;
-            } else if (num < 15.f) {
-                R = 153;
-                G = 87;
-                B = 0;
-            } else {
-                R = 106;
-                G = 52;
-                B = 3;
-            }
-        }
-
+    {  
+		switch (palette)
+		{
+		case 1:		  // domyslna paleta by tomek
+		{
+			if (num == iterations) {    // tu zaczyna sie paleta kolorow
+				this->R = 0;            // ktorej nie bede komentowal bo
+				this->G = 0;            // bo to nie ma sensu
+				this->B = 0;			// ale jesli pixel przetrwal wszystkie iteracje to koloruje go na czarno
+			}
+			else {
+				num = (int)num % 16;
+				if (num < 1.f) {
+					this->R = 66;
+					this->G = 30;
+					this->B = 15;
+				}
+				else if (num < 2.f) {
+					this->R = 25;
+					this->G = 7;
+					this->B = 26;
+				}
+				else if (num < 3.f) {
+					this->R = 9;
+					this->G = 1;
+					this->B = 47;
+				}
+				else if (num < 4.f) {
+					this->R = 4;
+					this->G = 4;
+					this->B = 73;
+				}
+				else if (num < 5.f) {
+					this->R = 0;
+					this->G = 7;
+					this->B = 100;
+				}
+				else if (num < 6.f) {
+					this->R = 12;
+					this->G = 44;
+					this->B = 138;
+				}
+				else if (num < 7.f) {
+					this->R = 24;
+					this->G = 82;
+					this->B = 177;
+				}
+				else if (num < 8.f) {
+					this->R = 57;
+					this->G = 125;
+					this->B = 209;
+				}
+				else if (num < 9.f) {
+					this->R = 134;
+					this->G = 181;
+					this->B = 229;
+				}
+				else if (num < 10.f) {
+					this->R = 211;
+					this->G = 236;
+					this->B = 248;
+				}
+				else if (num < 11.f) {
+					this->R = 241;
+					this->G = 233;
+					this->B = 191;
+				}
+				else if (num < 12.f) {
+					this->R = 248;
+					this->G = 201;
+					this->B = 95;
+				}
+				else if (num < 13.f) {
+					this->R = 255;
+					this->G = 170;
+					this->B = 0;
+				}
+				else if (num < 14.f) {
+					this->R = 204;
+					this->G = 128;
+					this->B = 0;
+				}
+				else if (num < 15.f) {
+					R = 153;
+					G = 87;
+					B = 0;
+				}
+				else {
+					R = 106;
+					G = 52;
+					B = 3;
+				}
+			}
+			break;
+		};
+		case 2:			   // prostsza testowa paleta
+		{
+			if (num == iterations) {
+				this->R = 0;
+				this->G = 0;
+				this->B = 0;
+			}
+			else {
+				num = (int)num % 5;
+				if (num < 1.f) {
+					this->R = 93;
+					this->G = 183;
+					this->B = 222;
+				}
+				else if (num < 2.f) {
+					this->R = 208;
+					this->G = 83;
+					this->B = 83;
+				}
+				else if (num < 3.f) {
+					this->R = 249;
+					this->G = 154;
+					this->B = 187;
+				}
+				else if (num < 4.f) {
+					this->R = 22;
+					this->G = 168;
+					this->B = 199;
+				}
+				else {
+					R = 107;
+					G = 120;
+					B = 147;
+				}
+		
+			}
+			break;
+		}
+	
+		}
         mandelbrot <<this->R <<' ' <<this->G <<' ' <<this->B <<'\n';    // wypisanie koloru do pliku
     }
 };
